f2ceee9ba1d4dc534f930f9f404df9021b737945
Thomas Seifert <thomas.seifert@mysnip.de>
2013-01-06 19:33:22 +0100

    initial commit comment

---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Default_IndexController extends Zend_Controller_Action
+{
+
+    /**
+     * Index action for the direct page access and handling login
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        /* To be done */
+    }
+
+
+}
+
